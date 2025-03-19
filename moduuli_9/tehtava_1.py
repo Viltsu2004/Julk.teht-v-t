62ab65926e82fe1578bac9890fdae301828e21c8
ville-valtterihaapa-aho <villvhaa@metropolia.fi>
2025-03-19 10:30:37 +0200

    Lisäsin teht 1

--- moduuli_9/tehtava_1.py
+++ moduuli_9/tehtava_1.py
@@ -1,5 +1,12 @@
-print("onnistuuko")
-print("onnistuu")
-print("ei onnistu")
-print("ei vieläkään")
-print("taisi onnistua")
+class Auto:
+    def __init__(self, rekisteritunnus, huippunopeus, nopeus=0, kuljettumatka=0):
+        self.rekisteritunnus = rekisteritunnus
+        self.huippunopeus = huippunopeus
+        self.nopeus = nopeus
+        self.kuljettumatka = kuljettumatka
+
+
+auto1 = Auto("ABC-123", 142)
+
+print(f"Auton rekisteritunnus on {auto1.rekisteritunnus:s}, sen huippunopeus on {auto1.huippunopeus} km/h, \ntämän hetkinen nopeus {auto1.nopeus:d} km/h ja kuljettu matka {auto1.kuljettumatka:d} km")
+
