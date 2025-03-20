e85d8500b7bb847d27900602c084db5c838ddaae
ville-valtterihaapa-aho <villvhaa@metropolia.fi>
2025-03-20 11:13:35 +0200

    Uuusi Push

---
+++ moduuli_9/tehtava_4.py
@@ -0,0 +1,16 @@
+
+import random
+
+def main():
+    auto = []
+    for autot in range(10):
+        auto.append(f"ABC-{autot},  {random.randint(100, 200)}")
+
+class Auto:
+    def __init__(self, rekisteri, maksiminopeus, nopeus, matka):
+        self.rekisteri = rekisteri
+        self.maksiminopaus = maksiminopeus
+        self.nopeus = nopeus
+        self.matka = matka
+
+main()jea
